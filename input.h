d86f5a2843d03e44d3753d29843e95a919aa6524
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-03-13 15:37:49 +0300

    Create input.h

---
+++ input.h
@@ -0,0 +1,14 @@
+//
+// Created by yura on 28.02.2021.
+//
+
+#ifndef LAB2_SEM2_INPUT_H
+#define LAB2_SEM2_INPUT_H
+
+
+void input_int(int*);
+void input_three(char[10], int*, int*);
+
+
+
+#endif //LAB2_SEM2_INPUT_H
