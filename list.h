a9e655ef44b06f9b5e645bfe6ca04c2117e39799
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-03-20 14:55:02 +0300

    Create list.h

---
+++ list.h
@@ -0,0 +1,20 @@
+//
+// Created by yura on 14.03.2021.
+//
+
+#ifndef LAB2_SEM2_LIST_H
+#define LAB2_SEM2_LIST_H
+
+#include "input.h"
+
+
+
+List *queue_list_input_p(List*, int);
+List *queue_list_input(List*, int);
+Massive queue_list_output_p(Massive, int);
+List *queue_list_output(List*);
+List *queue_list_print(List*);
+void del_list(Massive mass);
+Item *item_input(int );
+
+#endif //LAB2_SEM2_LIST_H
