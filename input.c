7a2e3e4f2bcd68ccfb469cb38e3c35b566ac2790
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-03-20 14:54:46 +0300

    Update input.c

--- input.c
+++ input.c
@@ -20,7 +20,7 @@ void input_int(int *a){
     } while (n <= 0);
 }
 
-void input_three(char id[10], int *ta, int *ts){
+void input_three(char id[10], int *ta, int *ts, int time){
     int n;
     do{
         n = scanf(" %s%d%d", id, ta, ts);
@@ -32,6 +32,8 @@ void input_three(char id[10], int *ta, int *ts){
             printf("You`re wrong! Service time can`t be 0!");
             scanf("%*c");
         }
-    } while (n < 3);
+        if (*ta > time){
+            printf("Please, repeat again! Your flight hasn't arrived yet!\n");
+        }
+    } while (n < 3 || *ta > time);
 }
-
